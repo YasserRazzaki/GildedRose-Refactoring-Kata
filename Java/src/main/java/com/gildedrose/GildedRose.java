fc19339afee39911e740779baeff06075e7495da
Yasser <yasserrazzaki0@gmail.com>
2026-06-02 12:42:16 +0200

    refactor: continue method extraction in GildedRose

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,18 +12,17 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(Backstage)) {
+            if (!isAgedBrie(item)
+                && !isBackstage(item)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals(Sulfuras)) {
-                        item.quality--;
+                    if (!isSulfuras(item)) {
+                        decreaseQuality(item);
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality++;
-
-                    if (item.name.equals(Backstage)) {
+                    increaseQuality(item);
+                    if (isBackstage(item)) {
                         if (item.sellIn < 11) increaseQuality(item);
 
                         if (item.sellIn < 6) increaseQuality(item);
@@ -31,16 +30,16 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals(Sulfuras)) {
+            if (!isSulfuras(item)) {
                 item.sellIn--;
             }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(Backstage)) {
+            if (isExpired(item)) {
+                if (!isAgedBrie(item)) {
+                    if (!isBackstage(item)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals(Sulfuras)) {
-                                item.quality--;
+                            if (!isSulfuras(item)) {
+                                decreaseQuality(item);
                             }
                         }
                     } else {
@@ -52,9 +51,30 @@ class GildedRose {
         }
     }
 
+    private static void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+        item.quality--; }
+    }
+
+    private static boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private static boolean isSulfuras(Item item) {
+        return item.name.equals(Sulfuras);
+    }
+
+    private static boolean isBackstage(Item item) {
+        return item.name.equals(Backstage);
+    }
+
+    private static boolean isAgedBrie(Item item) {
+        return item.name.equals(AGED_BRIE);
+    }
+
     private static void increaseQuality(Item item) {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
-    }
-}
+
+}}
