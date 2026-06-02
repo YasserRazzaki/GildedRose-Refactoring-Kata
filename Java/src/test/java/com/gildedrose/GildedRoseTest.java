0ecec5ba8665dcdf3dc9640b89a6f9ba8406f8d0
Yasser <yasserrazzaki0@gmail.com>
2026-06-02 10:40:51 +0200

    test: adding first two tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,43 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("fixme", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
 
+    private Item[] createItems(Item... items) {
+        return items;
+    }
+
+    private void updateQuality(Item[] items) {
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+    }
+
+    @Test
+    @DisplayName("qualité et sellIn diminuent de 1 chaque jour")
+    void quality_and_sellIn_decrease_by_1() {
+        Item[] items = createItems(new Item("Normal Item", 10, 20));
+        updateQuality(items);
+        assertEquals(9, items[0].sellIn);
+        assertEquals(19, items[0].quality);
+    }
+
+    @Test
+    @DisplayName("qualité diminue 2x plus vite après date")
+    void quality_decreases_twice_as_fast_after_sell_date() {
+        Item[] items = createItems(new Item("Nouveau Item", 0, 20));
+        updateQuality(items);
+        assertEquals(-1, items[0].sellIn);
+        assertEquals(18, items[0].quality);
+    }
 }
